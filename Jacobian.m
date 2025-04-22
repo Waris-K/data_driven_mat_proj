9f4b447b4e2aaa611eed69adc76640438e70febc
Waris Khan <142842012+Waris-K@users.noreply.github.com>
2025-04-22 14:43:03 -0400

    Delete Jacobian.m

--- Jacobian.m
+++
@@ -1,5 +0,0 @@
-% Function to compute Jacobian matrix
-function J = Jacobian(elemNodes, dN_dxi, dN_deta)
-    J = [dN_dxi * elemNodes(:, 1), dN_dxi * elemNodes(:, 2);
-         dN_deta * elemNodes(:, 1), dN_deta * elemNodes(:, 2)];
-end
